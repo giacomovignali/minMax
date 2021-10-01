d631bde21fb4ec235c14e4be51e2f4250fa56a4d
Giacomo Vignali <GIACOMO.VIGNALI@local.ittcesena.it>
2021-10-01 09:31:49 +0200

    V1.1: aggiunta calcolo numero minimo

--- minMax/Program.cs
+++ minMax/Program.cs
@@ -6,7 +6,19 @@ namespace minMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vettore = new int[5] { 3, 5, 2, 4, 9 };
+
+            int min = vettore[0];
+
+            for(int i = 0; i < 5; i++)
+            {
+                if(vettore[i] < min)
+                {
+                    min = vettore[i];
+                }
+            }
+
+            Console.WriteLine(min);
         }
     }
 }
